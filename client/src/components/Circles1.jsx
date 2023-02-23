4b98a7775661dc86e472dbd3d928b4abce44f590
Dydy-T83 <rudy-t83@hotmail.fr>
2023-02-23 10:00:23 +0100

    push rudy avec les 3 requêtes spéciales et qui s'affiche cotés client

--- client/src/components/Circles1.jsx
+++ client/src/components/Circles1.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { publicRequest } from "../requestMethods";
 
 function Circles1() {
     const [id, setId] = useState("");
